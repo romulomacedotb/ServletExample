af31b6a613ad71fb0d2450f26a1cb2dd88087fc4
Bull@# <romulo@projedata.com.br>
2023-08-07 19:55:04 -0300

    Mysql Config Example

--- src/main/java/servlets/util/DatabaseUtil.java
+++ src/main/java/servlets/util/DatabaseUtil.java
@@ -5,6 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
+	//mysql 
+	//String URL = "jdbc:Mysql://localhost:3306/seu_banco_de_dados";
+	// Class = com.mysql.cj.jdbc.Driver
+	
     private static final String URL = "jdbc:postgresql://localhost:5432/teste_new";
     private static final String USER = "postgres";
     private static final String PASSWORD = "91095850";
